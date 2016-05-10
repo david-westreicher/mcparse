9af8d319d3510653045ebafb5acdb8033a345bb2
David Westreicher <david.westreicher@gmail.com>
2016-05-10 21:59:45 +0200

    add callgraph generation

---
+++ src/callgraph.py
@@ -0,0 +1,51 @@
+from .utils import function_ranges
+
+
+def bbstocallgraph(bbs, verbose=0, dotfile=None):
+    # create callgraph
+    fun_ranges = function_ranges(bbs)
+    callgraph = {name: set() for name in fun_ranges}
+    for currfun in fun_ranges:
+        start, end = fun_ranges[currfun]
+        for bb in bbs[start:end]:
+            for op, _, _, fname in bb:
+                if op == 'call':
+                    callgraph[currfun].add(fname)
+
+    # global implicitly calls main in the end
+    if 'main' in fun_ranges and '__global__' in fun_ranges:
+        callgraph['__global__'].add('main')
+
+    if verbose > 0:
+        print('\n' + ' Call Graph '.center(40, '#'))
+        for el in callgraph:
+            print(str(el).ljust(10) + '\t->\t' + (', '.join([str(el) for el in callgraph[el]])))
+
+    if dotfile is not None:
+        from .utils import makeDotFile
+        makeDotFile(dotfile, bbs)
+
+    return callgraph
+
+if __name__ == '__main__':
+    import argparse
+    from .parser import parsefile
+    from .three import asttothree
+    from .bb import threetobbs
+    from .lvn import lvn
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="The *.mc file to convert into a Call Graph")
+    parser.add_argument('dotfile', default=None)
+    parser.add_argument('--lvn', '-l', action='count', default=False)
+    parser.add_argument('--verbose', '-v', action='count', default=0)
+    args = parser.parse_args()
+    bbs = threetobbs(
+        asttothree(
+            parsefile(
+                args.filename,
+                verbose=args.verbose - 2),
+            verbose=args.verbose - 1),
+        verbose=1 if not args.lvn else 0)
+    if args.lvn:
+        bbs = lvn(bbs, verbose=1)
+    bbstocallgraph(bbs, args.verbose + 1, args.dotfile)
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+
 all_ops = [
     'jump',
     'jumpfalse',
@@ -52,3 +53,83 @@ def isvar(arg):
     if arg.startswith('default-'):
         return False
     return True
+
+
+def function_ranges(bbs):
+    '''
+    Generates the following dictionary:
+        {
+            "fun1":         [0,  10],
+            "fun2":         [10, 20],
+            "__global__":   [20, 24]
+        }
+    '''
+    functions = {}
+    currfunc = '__global__'
+    for blocknum, bb in enumerate(bbs):
+        for op, _, _, fname in bb:
+            if op == 'function':
+                currfunc = fname
+        if currfunc not in functions:
+            functions[currfunc] = [blocknum, blocknum + 1]
+        else:
+            functions[currfunc][1] = blocknum + 1
+    return functions
+
+
+def makeDotFile(dotfile, bbs, ctrlflowgraph=None):
+    if ctrlflowgraph is None:
+        from .cfg import bbstocfg
+        ctrlflowgraph = bbstocfg(bbs, verbose=1)
+
+    def gen_block_calls_fun(bbs):
+        block_calls_fun = {i: set() for i in range(len(bbs))}
+        for i, bb in enumerate(bbs):
+            for op, _, _, fname in bb:
+                if op == 'call':
+                    block_calls_fun[i].add(fname)
+        if 'main' in fun_ranges and '__global__' in fun_ranges:
+            last_glob_block = fun_ranges['__global__'][1]
+            block_calls_fun[last_glob_block].add('main')
+        return block_calls_fun
+
+    def bb_to_dotnode(bb, num, children):
+        import cgi
+        from .three import prettythreestr
+        dotnode = 'node [shape=plaintext]\n'
+        html = ['<<TABLE BORDER="1" CELLBORDER="0" CELLSPACING="0">']
+        for op, arg1, arg2, res in bb:
+            if op in ['end-fun', 'function']:
+                continue
+            html.append('<TR>')
+            parts = cgi.escape(prettythreestr(op, arg1, arg2, res)).split('\t')
+            for part in parts:
+                html.append('<TD>' + part + '</TD>')
+            for _ in range(5 - len(parts)):
+                html.append('<TD></TD>')
+            html.append('</TR>')
+        html.append('</TABLE>>')
+        dotnode += str(num) + ' [label=' + '\n'.join(html) + '];\n'
+        for child in children:
+            dotnode += '%s -> %s;\n' % (num, child)
+        return dotnode
+
+    fun_ranges = function_ranges(bbs)
+    block_calls_fun = gen_block_calls_fun(bbs)
+
+    with open(dotfile, 'w') as f:
+        f.write('digraph R {\n')
+        for cluster, fun in enumerate(fun_ranges):
+            f.write('subgraph cluster%d {\n' % cluster)
+            startblock, endblock = fun_ranges[fun]
+            for i in range(startblock, endblock):
+                bb = bbs[i]
+                children = ctrlflowgraph[i]
+                bbnode = bb_to_dotnode(bb, i, children)
+                f.write(bbnode.replace('\n', '\n\t'))
+            f.write('label = "%s";\n' % fun)
+            f.write('}\n')
+        for block, fnames in block_calls_fun.items():
+            for fname in fnames:
+                f.write('%s -> %s;\n' % (block, fun_ranges[fname][0]))
+        f.write('}')
